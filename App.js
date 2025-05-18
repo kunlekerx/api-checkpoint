25938b6bbcb5e9a7b7f157f31bbffb3fa3e858de
kunlekerx <habibajibola02@gmail.com>
2025-05-18 22:53:18 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserList from './UserList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>📋 React Axios User List</h1>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
